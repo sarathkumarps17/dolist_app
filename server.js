f5c17d5e8926d9337b29b2d0817892f28e2aed56
sarathkumarps17 <sarathkumarps@outlook.com>
2020-09-08 00:29:49 +0530

    prepareing for deployment

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connect = require("./config/db/db");
 
+const path = require("path");
+
 const app = express();
 connect();
 app.use(express.json({ extended: false }));
@@ -9,7 +11,15 @@ const routes = require("./routes/routes");
 
 app.use(routes);
 
-const PORT = 5000 || process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
